cd5a635daebb6d172f8abd2adccff1ab69fc0f7e
Michael David Hall <60527849+Soulstealer07@users.noreply.github.com>
2020-03-15 17:48:06 -0500

    Update MuslceMass.R

--- MuslceMass.R
+++ MuslceMass.R
@@ -19,3 +19,28 @@ plot(quadmodel$fitted.values,quadmodel$residuals,col="red",pch=20)
 #Is the model a good fit?
 plot(quadmodel)
 
+#Are the variables normally distributed
+par(mfrow=c(1,1))
+qqnorm(MuscleMass$Age,col="red",pch=20,main="Normal Q-Q Plot Age")
+qqline(MuscleMass$Age,col="steelblue",lwd=2)
+qqnorm(MuscleMass$MuscleMass,col="red",pch=20,main="Normal Q-Q Plot Muscle Mass")
+qqline(MuscleMass$MuscleMass,col="steelblue",lwd=2)
+#histogram
+hist(x,col="blue")
+hist(y,col="red")
+plot(y,type="o",lwd=3,col="red",xlab="Age of Woman",main="Over Plot Muscle Mass vs. Age")
+
+install.packages("ggpubr")
+library(ggpubr)
+MuscleMass_Group
+MuscleMass_Group<-MuscleMass_Group[-c(2)]
+MuscleMass_Group<-as.data.frame(MuscleMass_Group)
+MuscleMass_Group
+str(MuscleMass_Group)
+MuscleMass_Group$Group <- ordered(MuscleMass_Group$Group,levels=c("Age40","Age50","Age60","Age70"))
+levels(MuscleMass_Group$Group)
+
+ggboxplot(MuscleMass_Group, x = "Group", y = "MuscleMass", 
+          color = "Group", palette = c("red", "blue","black","green"),
+          order = c("Age40", "Age50", "Age60","Age70"),
+          ylab = "MuscleMass", xlab = "Age")
